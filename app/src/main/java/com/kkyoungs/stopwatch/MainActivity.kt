9b10f7bb65c61ae5a78d22a40b53be42dc8b989e
eunkyoung99 <eunjee001@gmail.com>
2023-10-06 17:02:48 +0900

    Initial commit

---
+++ app/src/main/java/com/kkyoungs/stopwatch/MainActivity.kt
@@ -0,0 +1,157 @@
+
+package com.kkyoungs.stopwatch
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.ViewModel
+import com.kkyoungs.stopwatch.ui.theme.StopWatchTheme
+import java.util.*
+import kotlin.concurrent.timer
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
+
+
+//기능
+class MainViewModel:ViewModel(){
+
+    private var time = 0
+    private var timerTask : Timer? = null
+
+    private val _isRunning = mutableStateOf(false)
+    val isRunning: State<Boolean> = _isRunning
+
+    private val _sec = mutableStateOf(0)
+    val sec: State<Int> = _sec
+
+    private val _milli = mutableStateOf(0)
+    val milli: State<Int> = _milli
+
+    private val _lapTimes= mutableStateOf(mutableListOf<String>())
+    val lapTimes : State<List<String>> = _lapTimes
+
+    private var lap = 1
+
+    fun start(){
+        _isRunning.value = true
+
+        timerTask = timer(period = 10){
+            time ++
+            _sec.value = time/100
+            _milli.value = time % 100
+        }
+    }
+
+    fun pause(){
+        _isRunning.value = false
+        timerTask?.cancel()
+    }
+    fun reset(){
+        timerTask?.cancel()
+        time = 0
+        _isRunning.value = false
+        _sec.value = 0
+        _milli.value= 0
+
+        _lapTimes.value.clear()
+        lap = 1
+
+    }
+    fun recordLapTime(){
+        _lapTimes.value.add(0, "$lap Lap : ${sec.value}. ${milli.value}")
+        lap++
+    }
+}
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen(
+    sec: Int,
+    milli: Int,
+    isRunning:Boolean,
+    lapTimes : List<String>,
+    onReset : () -> Unit,
+    onToggle : (Boolean) -> Unit,
+    onLapTimer: () -> Unit,
+){
+    Scaffold(
+        topBar = {MediumTopAppBar(title = { Text("stopWatch")}) }
+    ){
+        Column (
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+
+                ){
+            Spacer(modifier = Modifier.height(40.dp))
+            Row() {
+                Text(text = "$sec", fontSize = 100.sp)
+                Text(text = "$milli")
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            Column (
+                modifier = Modifier
+                    .weight(1f)
+                    .verticalScroll(rememberScrollState()),
+                    ){
+                lapTimes.forEach{
+                    lapTimes -> Text(text = lapTimes)
+                }
+                
+            }
+            Row(modifier = Modifier
+                .padding(8.dp)
+                .fillMaxWidth(), horizontalArrangement =  Arrangement.SpaceBetween, verticalAlignment = Alignment.CenterVertically) {
+                
+                FloatingActionButton(
+                    onClick = { onReset() },
+                    Modifier.background(color = androidx.compose.ui.graphics.Color.Red)
+                ) {
+                    Image(painter = painterResource(id = R.drawable.baseline_refresh_24), contentDescription = "reset")
+
+                }
+                FloatingActionButton(
+                    onClick = { onToggle(isRunning) },
+                    Modifier.background(color = androidx.compose.ui.graphics.Color.Green)
+                ) {
+                    Image(painter = painterResource(id = if (isRunning)R.drawable.baseline_pause_circle_outline_24 else R.drawable.baseline_play_circle_outline_24), contentDescription = "start/pause")
+
+                }
+                Button(onClick = {
+                    onLapTimer()
+                }) {
+                    Text(text = "랩타임")
+                    
+                }
+                
+                
+            }
+        }
+    }
+        
+
+}
